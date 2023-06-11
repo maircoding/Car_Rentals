9a7f1dc76f6e59c045a06b54a35a134d1e583508
Mohammad Umair <31861803+mdumair0@users.noreply.github.com>
2023-06-12 01:53:42 +0530

    Update MainContent.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -9,8 +9,8 @@ function MainContent() {
                     <h1 className='py-2 font-bold text-center text-5xl p-4 md:text-left md:text-6xl md:p-0'>Save <span className='text-colour'>big</span> with our car rental</h1>
                     <h4 className='py-2 text-slate-400 text-center my-10 md:text-left'>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</h4>
                     <div className='py-8 flex flex-col md:flex-row gap-4'>
-                        <div className='py-4 px-4 md:py-4 md:px-10 bg-colour hover:cursor-pointer text-center hover:text-colour hover:bg-white hover:border-b-4 border-colour text-white rounded'>Book Ride</div>
-                        <div className='py-4 px-4 md:py-4 md:px-10 bg-slate-900 hover:cursor-pointer text-center hover:text-black hover:bg-white hover:border-b-4 border-slate-900 text-white rounded'>Learn More {'>'}</div>
+                        <a href="#book"><div className='py-4 px-4 md:py-4 md:px-10 bg-colour hover:cursor-pointer text-center hover:text-colour hover:bg-white hover:border-b-4 border-colour text-white rounded'>Book Ride</div></a>
+                        <a href="#choose"><div className='py-4 px-4 md:py-4 md:px-10 bg-slate-900 hover:cursor-pointer text-center hover:text-black hover:bg-white hover:border-b-4 border-slate-900 text-white rounded'>Learn More {'>'}</div></a>
                     </div>
                 </div>
                 <div className='hidden md:block'>
